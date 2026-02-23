f706af0b89e640364663bb81fb6739d5a5bb529d
Jacob Clayden <me@jacobcx.dev>
2026-02-23 08:00:35 +0000

    fix(08-01): add @available annotations to @Observable classes
    - Add @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *) to Counter,
      Parent, Child, MultiTracker in ObservationModels.swift
    - No @Observable classes found in fuse-app (only @ObservableState structs)
    - 91 fuse-library tests still pass

--- examples/fuse-library/Sources/FuseLibrary/ObservationModels.swift
+++ examples/fuse-library/Sources/FuseLibrary/ObservationModels.swift
@@ -4,6 +4,7 @@
 import Foundation
 import Observation
 
+@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
 @Observable public class Counter {
     public var count: Int = 0
     @ObservationIgnored public var ignoredValue: Int = 0
@@ -14,6 +15,7 @@ import Observation
     public init() {}
 }
 
+@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
 @Observable public class Parent {
     public var name: String = ""
     public var child: Child = Child()
@@ -21,12 +23,14 @@ import Observation
     public init() {}
 }
 
+@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
 @Observable public class Child {
     public var value: Int = 0
 
     public init() {}
 }
 
+@available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
 @Observable public class MultiTracker {
     public var alpha: Int = 0
     public var beta: String = ""
